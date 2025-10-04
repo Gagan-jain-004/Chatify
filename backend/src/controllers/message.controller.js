8d4c6e366681a21a0b09991e022ab7a0075127a6
Gagan jain004 <gagan.praveen004@gmail.com>
2025-10-04 13:04:43 +0530

    #Sidebar controller done

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,16 @@
+import User from "../models/user.model.js";
 
-export const getUsersForSidebar =(req,res)=>{
+export const getUsersForSidebar =async(req,res)=>{
  
-    
+    try {
+        const loggedInUserId = req.user._id;                     // this req content will get by protectroute middleware
+        const filteredUsers = await User.find({_id: {$ne: loggedInUserId}}).select("-password");       // getting all user except loginned User for sidebar and not to send pass to user
+
+        res.status(200).json(filteredUsers);
+
+    } catch (error) {
+        console.error("Error in getUsersForSidebar: ",error.message);
+        res.status(500).json({error:"Internal Server error "})
+        
+    }
 }
