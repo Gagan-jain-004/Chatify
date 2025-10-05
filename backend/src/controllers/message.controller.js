a39817f15da9303e7d60519fca88a0aa858c69b4
Gagan jain004 <gagan.praveen004@gmail.com>
2025-10-05 21:15:58 +0530

    #Completed

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js"
+import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getUsersForSidebar =async(req,res)=>{
  
@@ -62,6 +64,11 @@ export const sendMessage =async(req,res)=>{
         await newMessage.save();
 
         // todo: realtime functionaliy by socket.io
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
         res.status(201).json(newMessage)
 
     } catch (error) {
